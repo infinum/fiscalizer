8aa9a11c0a83b57778af3a96f7760c7cd2d00971
Vladimir Rosancic <vladimir.rosancic@infinum.hr>
2017-01-16 18:22:34 +0100

    added security code generator

--- lib/new/security_code_generator.rb
+++ lib/new/security_code_generator.rb
@@ -8,25 +8,22 @@ module Fiscalizer
     attr_reader :invoice, :private_key
 
     def call
-
+      invoice.security_code = md5_digest
     end
 
     private
 
-    def generate_security_code
-      # Build data set to generate security code
-      unsigned_code = ""
-      unsigned_code += invoice.pin
-      unsigned_code += invoice.time_issued_str " "
-      unsigned_code += invoice.issued_number.to_s
-      unsigned_code += invoice.issued_office.to_s
-      unsigned_code += invoice.issued_machine.to_s
-      unsigned_code += invoice.summed_total_str
-      # Sign with my private key
-      signed_code = OpenSSL::PKey::RSA.new(key_private).sign(OpenSSL::Digest::SHA1.new, unsigned_code)
-      # Create a MD5 digest from it
-      md5_digest = Digest::MD5.hexdigest(signed_code)
-      invoice.security_code = md5_digest
-    end # generate_security_code
+    def md5_digest
+      Digest::MD5.hexdigest(signed_code)
+    end
+
+    def signed_code
+      OpenSSL::PKey::RSA.new(private_key).sign(OpenSSL::Digest::SHA1.new, unsigned_code)
+    end
+
+    def unsigned_code
+      invoice.pin + invoice.time_issued_str('') + invoice.issued_number.to_s +
+        invoice.issued_office.to_s + invoice.issued_machine.to_s + invoice.summed_total_str
+    end
   end
 end
