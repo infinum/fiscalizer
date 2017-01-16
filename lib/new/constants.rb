32163a7cfa125a577c685affd4754e74d1d2c7e6
Vladimir Rosancic <vladimir.rosancic@infinum.hr>
2017-01-16 18:22:43 +0100

    added constants

---
+++ lib/new/constants.rb
@@ -0,0 +1,8 @@
+module Fiscalizer
+  module Constants
+    FISCALIZATION_URL = 'https://cis.porezna-uprava.hr:8449/FiskalizacijaService'
+    TNS = 'http://www.apis-it.hr/fin/2012/types/f73'
+    XSI = 'http://www.w3.org/2001/XMLSchema-instance'
+    SCHEMA_LOCATION = 'http://www.apis-it.hr/fin/2012/types/f73 FiskalizacijaSchema.xsd'
+  end
+end
