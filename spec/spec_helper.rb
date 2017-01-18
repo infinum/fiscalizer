40db9f566f53264b64daaf862526200ab4e27e28
Vladimir Rosancic <vladimir.rosancic@infinum.hr>
2017-01-18 16:55:35 +0100

    removed unused require from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,8 @@ require 'fiscalizer'
 require 'pathname'
 require 'pry'
 require 'securerandom'
-require 'rspec/xsd'
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 root_path = Pathname.new(File.expand_path('../', File.dirname(__FILE__)))
 Dir[root_path.join('spec/support/**/*.rb')].each { |f| require f }
-
-RSpec.configure do |config|
-  config.include RSpec::XSD
-end
