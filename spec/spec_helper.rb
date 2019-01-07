0734f115dbe6f41bb639068743c0314e5ce188b2
nb <nikola.buhinicek@infinum.hr>
2019-01-07 14:03:42 +0100

    Fix specs using webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,15 @@ require 'fiscalizer'
 require 'pathname'
 require 'pry'
 require 'securerandom'
+require 'webmock/rspec'
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 root_path = Pathname.new(File.expand_path('../', File.dirname(__FILE__)))
 Dir[root_path.join('spec/support/**/*.rb')].each { |f| require f }
+
+public
+
+def get_response_file(file)
+  "spec/support/responses/#{file}"
+end
